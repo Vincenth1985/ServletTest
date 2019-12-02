642b285a5be688a15221f829e9c8a4c69119b6fb
Vincent Honca <vincenth1985@gmail.com>
2019-12-02 09:42:34 +0100

    creating gitignore

---
+++ src/main/java/be/intecbrussel/QueryServlet.java
@@ -0,0 +1,37 @@
+package be.intecbrussel;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/QueryServlet")
+public class QueryServlet extends HttpServlet {
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("text/plain");
+
+        String param = "param";
+        String paramValue = req.getParameter(param);
+
+        if (paramValue == null) {
+            out.write("Hello Pluto");
+        } else {
+
+            out.write(paramValue);
+        }
+
+
+        out.close();
+    }
+}
